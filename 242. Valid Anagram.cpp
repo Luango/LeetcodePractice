c7de363c89509eb8332084056d04f2fd1b01bd12
Luango <luangreturn@gmail.com>
2017-03-23 19:39:10 -0400

    Update 242. Valid Anagram.cpp

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -1,3 +1,9 @@
+#Given two strings s and t, write a function to determine if t is an anagram of s.
+
+#For example,
+#s = "anagram", t = "nagaram", return true.
+#s = "rat", t = "car", return false.
+
 bool isAnagram(string s, string t) {
     if (s.length() != t.length()) return false;
     int n = s.length();
