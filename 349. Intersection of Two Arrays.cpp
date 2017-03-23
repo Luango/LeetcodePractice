6377d6df6ea795b439c1ffa6a748788230f656a5
Luango <luangreturn@gmail.com>
2017-03-22 21:03:46 -0400

    Update 349. Intersection of Two Arrays.cpp

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -1,3 +1,8 @@
+#Given two arrays, write a function to compute their intersection. 
+#Example:
+#Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+
 vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
     set<int> s(nums1.begin(), nums1.end());
     vector<int> out;
