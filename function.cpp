47399db528d39d71dbf641ffd2ed0b372aff28ba
Luango <luangreturn@gmail.com>
2017-03-22 20:39:21 -0400

    Add files via upload

--- function.cpp
+++ function.cpp
@@ -1,8 +1,6 @@
-vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    set<int> s(nums1.begin(), nums1.end());
-    vector<int> out;
-    for (int x : nums2)
-        if (s.erase(x))
-            out.push_back(x);
-    return out;
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+    nums1.erase(set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums1.begin()), nums1.end());
+    return nums1;
 }
